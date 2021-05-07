b848f941f5358be3873e0a7b1d65ddc37132f241
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-06 21:34:32 -0700

    Importing bootstrap.min.css in order to use styling in character table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import TitleCard from './components/TitleCard/TitleCard';
+import CharacterTable from './components/CharacterTable/CharacterTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <TitleCard />
+      <CharacterTable />
     </div>
   );
 }
