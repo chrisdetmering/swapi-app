b017b3c4dd33792f4bef20747f8b6e940f0ed0f4
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-07 15:04:40 -0700

    Adding functions and state to component - changing CharacterTable from React.Component to function that returns Table element - adding allCharacters state to hold all characters returned by API on initial GET Adding useEffect to call getAllCharacters on load Adding getAllCharacters to get characters from API on load Adding characterTableRows function to convert characters table into table row elements replacing table rows with function characterTableRows

--- src/components/CharacterTable/CharacterTable.js
+++ src/components/CharacterTable/CharacterTable.js
@@ -1,33 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-class CharacterTable extends React.Component {
-    render() {
-        return (
-            <Table striped bordered variant="light" size="sm" style={{ width: '90vw', margin: 'auto'}}>
-                  <thead style={{ cursor: 'pointer'}}>
-                    <tr>
-                        <th>Name</th>
-                        <th>Birth Date</th>
-                        <th>Height</th>
-                        <th>Mass</th>
-                        <th>Homeworld</th>
-                        <th>Species</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>Luke Skywalker</td>
-                        <td>19BBY</td>
-                        <td>172</td>
-                        <td>77</td>
-                        <td>?</td>
-                        <td>?</td>
-                        </tr>
-                    </tbody>
-            </Table>
-        )
+const CharacterTable = () => {
+    const [allCharacters, setAllCharacters] = useState([]);
+
+    const BASE_URL = 'https://swapi.dev/api';
+
+    useEffect(() => {
+        getAllCharacters()
+    },[]);
+
+    const getAllCharacters = async () => {
+        const allCharacters = await axios({
+            url: `${BASE_URL}/people/`,
+            method: 'get'
+        })
+        setAllCharacters(allCharacters.data.results);
     }
+
+    const characterTableRows = () => {
+        console.log(allCharacters)
+        const rows = allCharacters.map((character, i) => {
+            console.log(character);
+            return (            
+                <tr key={i}>
+                    <th>{character.name}</th>
+                    <th>{character.birth_year}</th>
+                    <th>{character.height}</th>
+                    <th>{character.mass}</th>
+                    <th>{character.homeworld}</th>
+                    <th>{character.species}</th>
+                </tr>
+            )
+        })
+        return rows;
+    }
+
+    return (
+        <Table responsive striped bordered variant="light" size="sm" style={{ width: '90vw', margin: 'auto'}}>
+            <thead style={{ cursor: 'pointer'}}>
+                <tr>
+                    <th>Name</th>
+                    <th>Birth Date</th>
+                    <th>Height</th>
+                    <th>Mass</th>
+                    <th>Homeworld</th>
+                    <th>Species</th>
+                </tr>
+            </thead>
+            <tbody>
+                {characterTableRows()}
+            </tbody>
+        </Table>
+    )
 }
 
 export default CharacterTable;
