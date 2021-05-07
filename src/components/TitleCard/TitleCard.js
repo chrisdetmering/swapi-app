81d8e4ffab8c65cfa62fb1ac779169a7eb089289
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-06 21:33:50 -0700

    Fixing styling of title card after importing bootstrapp css file in App.js

--- src/components/TitleCard/TitleCard.js
+++ src/components/TitleCard/TitleCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import './TitleCard.css'
+import './TitleCard.css';
 
 class TitleCard extends React.Component {
     render() {
         return  (
-                <Card>
+                <Card className="text-center text-white">
                     <Card.Body>
                         <Card.Title><em>S</em>WAPI</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Star Wars API</Card.Subtitle>
+                        <Card.Subtitle className="mb-2">Star Wars API</Card.Subtitle>
                         <Card.Text>WHO'S WHO IN THE STAR WARS UNIVERSE</Card.Text>
                     </Card.Body>
                 </Card>
