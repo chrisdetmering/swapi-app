4b7c6e6fab29d03cf5f193f09b61e79e572da08e
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-12 15:54:39 -0700

    Adding function to disable buttons if there is no prev or next URL

--- src/components/TableComponents/NextPrevButton/NextPrevButton.js
+++ src/components/TableComponents/NextPrevButton/NextPrevButton.js
@@ -1,13 +1,37 @@
 import Button from 'react-bootstrap/Button';
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
+import {useEffect} from 'react';
+
 import './NextPrevButton.css';
 
-const NextPrevButton = () => {
+const NextPrevButton = (props) => {
+
+    useEffect(() => {
+        setButtonDisabledProperty()
+    }, [props.nextPageUrl, props.prevPageUrl])
+    
+    const handleClick = (event) => {
+        if(event.target.id === 'next'){
+            props.setCurrentApiUrl(props.nextPageUrl);
+            return
+        }
+        if(event.target.id === 'prev'){
+            props.setCurrentApiUrl(props.prevPageUrl);
+            return
+        }
+    }
+
+    const setButtonDisabledProperty = () => {
+        document.getElementById('next').disabled = !props.nextPageUrl
+        document.getElementById('prev').disabled = !props.prevPageUrl
+    }
+
+
     return (
         <div id="button-div">
-            <Button className="button" variant="light" size="sm"><BsFillCaretLeftFill /> Prev</Button>
-            <Button className="button" variant="light" size="sm">Next <BsFillCaretRightFill /></Button>
+            <Button id="prev" className="button" variant="light" size="sm" onClick={handleClick}><BsFillCaretLeftFill /> Prev</Button>
+            <Button id="next" className="button" variant="light" size="sm" onClick={handleClick}>Next <BsFillCaretRightFill /></Button>
         </div>
     )
 }
